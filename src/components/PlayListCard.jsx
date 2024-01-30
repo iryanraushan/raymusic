1c32e5ea336d40b14fed74e383b0a0237e3b4e3c
Ryan Raushan <raushankrpnhs2000@gmail.com>
2024-01-31 03:05:57 +0530

    Update playlist card width and grid layout

--- src/components/PlayListCard.jsx
+++ src/components/PlayListCard.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const PlayListCard = ({ title, id, image }) => {
   return (
-    <div class="min-[360px]:w-[130px] rounded overflow-hidden  transition duration-300 ease-in-out hover:shadow-[0_5px_5px_rgb(105,105,105,0.12)]  dark:hover:shadow-black/30 hover:scale-110">
+    <div class=" md:w-[200px] lg:w-[220px] rounded overflow-hidden  transition duration-300 ease-in-out hover:shadow-[0_5px_5px_rgb(105,105,105,0.12)]  dark:hover:shadow-black/30 hover:scale-110">
       <Link to={`/playlists/${id}`}>
-        <img class="w-full" src={image[2].link} alt="Sunset in the mountains" />
+        <img class="w-full" src={image[2].link} />
         <div class="px-6 py-4">
           <p class="text-[#ebebeb] text-[15px] text-center text-base whitespace-nowrap overflow-hidden overflow-ellipsis">
             {title.replace(/\(.*?\)/g, "").trim()}{" "}
